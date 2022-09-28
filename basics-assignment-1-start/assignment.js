9c6144a5341771f4bc45b5d36f8b2831bb03b44c
Preeti Negi <npreeti0999gmail.com>
2022-09-28 16:56:25 +0530

    initial commit

---
+++ basics-assignment-1-start/assignment.js
@@ -0,0 +1,13 @@
+//Create two variables 
+const userInput = 15;
+let result;
+
+//Chaange the variable values
+result = userInput + 18;
+result = result - 5;
+result = result * 4;
+result = result / 2;
+
+//Alert the values
+alert(result);
+alert(userInput);
