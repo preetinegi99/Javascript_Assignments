3076845d68450254519224e05cfae0c5e7e7c4da
Preeti Negi <npreeti0999gmail.com>
2022-09-29 19:15:53 +0530

    Assignment2 - Functions

---
+++ basics-assignment-2-start/assignment.js
@@ -0,0 +1,29 @@
+const task3Element = document.getElementById('task-3');
+
+//function1
+function wlcmMsg(){
+    alert("Welcome To Javascript!");
+}
+
+//function2
+function user(name){
+
+alert('Hi ' + name );    
+}
+
+//function3
+function dateOfBirth(date,month,year){
+    const DOB = date + '-' + month + "-" + year;
+    return DOB;
+    
+}
+//calling the functions
+wlcmMsg();
+user("Preeti");
+
+//addListener 
+task3Element.addEventListener('click', wlcmMsg);
+
+const DOB = dateOfBirth('9','Aug','1999');
+
+alert("Your date of birth is " + DOB);
